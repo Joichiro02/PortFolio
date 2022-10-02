d006b7225b928ed78a05cd7b58f02be791f3e625
markanthonyfamarin <markanthonyfamarin@gmail.com>
2022-10-02 09:59:44 +0800

    develop blog page

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -1,3 +1,5 @@
+import {blog, blog1, blog2} from "../../assets/images";
+import {MdKeyboardArrowRight} from "react-icons/md";
 import "./styles.scss";
 
 const Blog = () => {
@@ -5,7 +7,41 @@ const Blog = () => {
         <div className="blog">
             <h4 className="title">Blog</h4>
             <p className="description">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum</p>
-            
+            <div className="blogs">
+                <div className="blogContent">
+                    <img className="blogImg" src={blog} alt="" />
+                    <div className="infoContainer">
+                        <h6 className="title">How to make web tempates</h6>
+                        <p className="blogDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna </p>
+                        <div className="learnMore">
+                            <span className="text">Learn more</span>
+                            <MdKeyboardArrowRight className="arrow"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="blogContent">
+                    <img className="blogImg" src={blog1} alt="" />
+                    <div className="infoContainer">
+                        <h6 className="title">make Business card</h6>
+                        <p className="blogDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna </p>
+                        <div className="learnMore">
+                            <span className="text">Learn more</span>
+                            <MdKeyboardArrowRight className="arrow"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="blogContent">
+                    <img className="blogImg" src={blog2} alt="" />
+                    <div className="infoContainer">
+                        <h6 className="title">How to make Flyer Design</h6>
+                        <p className="blogDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna </p>
+                        <div className="learnMore">
+                            <span className="text">Learn more</span>
+                            <MdKeyboardArrowRight className="arrow"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,7 +2,37 @@ import "./styles.scss";
 
 const Contact = () => {
     return(
-        <div className="contact"></div>
+        <div className="contact">
+            <div className="upperContainer">
+                <div className="infoContainer">
+                    <h5 className="infoTitle">Leave us your info</h5>
+                    <form className="infoContent">
+                        <section className="formSection">
+                            <label className="labelName" htmlFor="">Your Full Name ( Required)</label>
+                            <input className="textField" type="text" />
+                        </section>
+                        <section className="formSection">
+                            <label className="labelName" htmlFor="">Your Email ( Required)</label>
+                            <input className="textField" type="text" />
+                        </section>
+                        <section className="formSection">
+                            <label className="labelName" htmlFor="">Subject   </label>
+                            <input className="textField" type="text" />
+                        </section>
+                        <section className="formSection">
+                            <label className="labelName" htmlFor="">Your Message</label>
+                            <textarea className="textArea" rows={11} ></textarea>
+                        </section>
+                        <button className="submit">send message</button>
+                    </form>
+                </div>
+                <div className="contactContainer">
+                    <h5 className="contactTitle">Contact information</h5>
+                    <div className="contactContent">
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
