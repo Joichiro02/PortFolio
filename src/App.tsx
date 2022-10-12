f967fd6a4287123dd4305407a15a83a66def8295
markanthonyfamarin <markanthonyfamarin@gmail.com>
2022-10-12 10:18:59 +0800

    continue developing theme color

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.scss';
+import "animate.css";
 import LeftContent from './components/leftContent/LeftContent';
 import RightContent from './components/rightContent/RightContent';
 import Blog from './pages/blog/Blog';
@@ -11,12 +12,27 @@ import Portfolio from './pages/portfolio/Portfoio';
 import Services from './pages/service/Service';
 import {AnimatePresence} from "framer-motion"
 import classNames from 'classnames';
-import { BsGearFill } from 'react-icons/bs';
+import { BsGearFill, BsMoonStars, BsPlusLg } from 'react-icons/bs';
+import { FaSun } from 'react-icons/fa';
 
 function App() {
   const location = useLocation();
-  const [themeMode, setThemeMode] = useState("lightMode");
+  const [themeMode, setThemeMode] = useState("darkMode");
   const [themeColor, setThemeColor] = useState("template-1");
+  const [openThemeCont, setOpenThemeCont] = useState(false);
+  const handleTheme = (theme : string) => {
+        if(theme === "lightMode"){
+            setThemeMode("lightMode");
+        }
+        else {
+            setThemeMode("darkMode");
+        }
+    }
+    const handleOpen = () => {
+      if(!openThemeCont){
+        setOpenThemeCont(!openThemeCont);
+      }
+    }
   return (
     <div
       className={classNames("App",{
@@ -39,27 +55,81 @@ function App() {
           </AnimatePresence>
         </div>
         <div className="gear">
-            <BsGearFill className="gearIcon"/>
-            <div className="colorsContainer">
+            <BsGearFill className="gearIcon" onClick={handleOpen}/>
+            <div className={classNames("colorsContainer",{
+              "open": openThemeCont
+            })}>
               <div className="headerContent">
-                <h6 className="header">Theme Template</h6>
-              </div>~
+                <h3 className="header">Theme Template</h3>
+                <BsPlusLg className="close" onClick={() => setOpenThemeCont(!openThemeCont)}/>
+              </div>
               <div className="modeContent">
-
+                <span className={classNames("sunContainer",{
+                  "active": themeMode === "lightMode"
+                })} onClick={() => handleTheme("lightMode")}>
+                  <FaSun className="sun"/>
+                </span>
+                <span className={classNames("moonContainer",{
+                  "active": themeMode === "darkMode"
+                })} onClick={() => handleTheme("darkMode")}>
+                  <BsMoonStars className="moon"/>
+                </span>
               </div>
               <div className="colorsContent">
-                <span onClick={() => setThemeColor("template-1")}></span>
-                <span onClick={() => setThemeColor("template-2")}></span>
-                <span onClick={() => setThemeColor("template-3")}></span>
-                <span onClick={() => setThemeColor("template-4")}></span>
-                <span onClick={() => setThemeColor("template-5")}></span>
-                <span onClick={() => setThemeColor("template-6")}></span>
-                <span onClick={() => setThemeColor("template-7")}></span>
+                <span className={classNames("templateContainer",{
+                  "active": themeColor === "template-1"
+                })} onClick={() => setThemeColor("template-1")}>
+                  <span className='template1'>
+                    <span className="animate__animated animate__rubberBand" ></span>
+                  </span>
+                </span>
+                <span className={classNames("templateContainer",{
+                  "active": themeColor === "template-2"
+                })} onClick={() => setThemeColor("template-2")}>
+                  <span className='template2'>
+                    <span className="animate__animated animate__rubberBand" ></span>
+                  </span>
+                </span>
+                <span className={classNames("templateContainer",{
+                  "active": themeColor === "template-3"
+                })} onClick={() => setThemeColor("template-3")}>
+                  <span className='template3'>
+                    <span className="animate__animated animate__rubberBand" ></span>
+                  </span>
+                </span>
+                <span className={classNames("templateContainer",{
+                  "active": themeColor === "template-4"
+                })} onClick={() => setThemeColor("template-4")}>
+                  <span className='template4'>
+                    <span className="animate__animated animate__rubberBand" ></span>
+                  </span>
+                </span>
+                <span className={classNames("templateContainer",{
+                  "active": themeColor === "template-5"
+                })} onClick={() => setThemeColor("template-5")}>
+                  <span className='template5'>
+                    <span className="animate__animated animate__rubberBand" ></span>
+                  </span>
+                </span>
+                <span className={classNames("templateContainer",{
+                  "active": themeColor === "template-6"
+                })} onClick={() => setThemeColor("template-6")}>
+                  <span className='template6'>
+                    <span className="animate__animated animate__rubberBand" ></span>
+                  </span>
+                </span>
+                <span className={classNames("templateContainer",{
+                  "active": themeColor === "template-7"
+                })} onClick={() => setThemeColor("template-7")}>
+                  <span className='template7'>
+                    <span className="animate__animated animate__rubberBand" ></span>
+                  </span>
+                </span>
               </div>
             </div>
         </div>
       </div>
-      <RightContent themeMode={themeMode} setThemeMode={setThemeMode}/>
+      <RightContent/>
     </div>
   );
 }
